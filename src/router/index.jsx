baa8cd295241cd714e81eca6f30fb5cd5f2e8c07
Augusto Gangui <augustogangui@gmail.com>
2024-05-28 02:03:16 -0300

    Categoria de problemas/Item Problema
    Se agregan imagenes tambien para el slideshow

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import { EmegencyPage } from "../pages/home/emergency.page.jsx";
 import { HomePage } from "../pages/home/home.page.jsx";
 import { ProblemsCategoryPage } from "../pages/home/problems.page.jsx";
 import { SearchResultPage } from "../pages/home/searchresult.page.jsx";
+import { CategoriaItemPage } from "../pages/categoria_item/categoria_item.page.jsx";
 
 export default createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export default createBrowserRouter([
   {
     path: "/problemas",
     element: <ProblemsCategoryPage />,
+  },
+  {
+    path: "/problemas/:id",
+    element: <CategoriaItemPage />,
   },
   {
     path: "/emergencia",
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "./counter/counterSlice.js";
 import searchReducer from "./searchResult/searchSlice.js"
+import categoriasReducer from './categoriasProblemas/categoriasProblemasSlice.js'
+
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
-        searchResult: searchReducer
+        searchResult: searchReducer,
+        categoriaProblemas: categoriasReducer
     },
 })
