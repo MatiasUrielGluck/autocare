c988ac52c27051c5a35503d47cd1adaa647d7bf5
Augusto Gangui <augustogangui@gmail.com>
2024-06-25 00:41:23 -0300

    se cambia vista de galeria

--- src/pages/categoria_item/categoria_item.page.jsx
+++ src/pages/categoria_item/categoria_item.page.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Card, CardContent, LinearProgress } from "@mui/material";
 import { HomeTemplate } from "../../templates/HomeTemplate";
 import Carousel from "react-material-ui-carousel";
+import { MediaView } from "../../components/MediaView";
 
 export const CategoriaItemPage = () => {
   const { id } = useParams();
@@ -29,24 +30,22 @@ export const CategoriaItemPage = () => {
       <Card
         sx={{
           display: "flex",
+          flexDirection: "column",
           // width: "100vw",
-          height: "50vh",
+          // height: "70vh",
           borderRadius: "1rem",
           padding: "1rem",
           margin: "2rem",
         }}
       >
-        <div style={{ textWrap: "wrap", overflowX: "auto" }}>
+        <div style={{ textWrap: "wrap", overflowX: "auto", maxHeight: "30vh" }}>
           <p>{categoriasStore.viewingCategoria.info}</p>
         </div>
-      </Card>
 
-      <Carousel swipe sx={{ margin: "2rem" }}>
-        {categoriasStore.viewingCategoria.images &&
-          categoriasStore.viewingCategoria.images.map((img, idx) => (
-            <img key={idx} src={`${img}`} style={{ height: "20vh" }} />
-          ))}
-      </Carousel>
+        {categoriasStore.viewingCategoria.gallery && (
+          <MediaView gallery={categoriasStore.viewingCategoria.gallery} />
+        )}
+      </Card>
     </HomeTemplate>
   );
 };
